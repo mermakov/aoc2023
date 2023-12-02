ecc3d576a35c081591e5178b8e333fefcc80ca63
Mikhail Ermakov <m.k.ermakov@gmail.com>
2023-12-02 21:08:04 +0100

    feat: day 2 part 2

--- aoc2023/2/__init__.py
+++ aoc2023/2/__init__.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 
 from aoc2023.utils import Solver
 
@@ -13,16 +14,33 @@ class PartOne(Solver):
     def __init__(self) -> None:
         super().__init__()
 
-    def _process_line(self, line: str) -> int | None:
+    def _get_dice_counts(self, line: str) -> tuple[int, dict[str, list[int]]]:
         game_info, rolls = line.split(":")
         game_id = int(game_info.split(" ")[-1])
 
+        dice_counts = defaultdict(list)
+
         for roll in rolls.split(";"):
-            dice_counts = _COUNT_PTN.findall(roll)
-            if any(
-                int(count) > _COLOR_LIMITS[color]
-                for count, color in dice_counts
-            ):
+            for count, color in _COUNT_PTN.findall(roll):
+                dice_counts[color].append(int(count))
+
+        return game_id, dice_counts
+
+    def _process_line(self, line: str) -> int | None:
+        game_id, dice_counts = self._get_dice_counts(line)
+        for color, counts in dice_counts.items():
+            if any(count > _COLOR_LIMITS[color] for count in counts):
                 return 0
 
         return game_id
+
+
+class PartTwo(PartOne):
+    def _process_line(self, line: str) -> int | None:
+        _, dice_counts = self._get_dice_counts(line)
+        power = 1
+        for color in _COLOR_LIMITS:
+            counts = dice_counts.get(color)
+            if counts:
+                power *= max(counts)
+        return power
