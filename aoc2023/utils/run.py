9c31078554b21a7c86811f924582d25d72a8f8a6
Mikhail Ermakov <m.k.ermakov@gmail.com>
2023-12-02 20:45:02 +0100

    feat: add basic infrastructure

---
+++ aoc2023/utils/run.py
@@ -0,0 +1,42 @@
+import argparse
+import importlib
+import sys
+from pathlib import Path
+
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-p", "--part", choices=(1, 2), type=int, default=1)
+    parser.add_argument("--test", action="store_true")
+    parser.add_argument("-i", "--input", type=Path)
+    parser.add_argument("day")
+
+    return parser.parse_args()
+
+
+def main():
+    opts = _parse_args()
+    solver_name = f"Part{'One' if opts.part == 1 else 'Two'}"
+
+    try:
+        module = importlib.import_module(f"aoc2023.{opts.day}")
+        solver = getattr(module, solver_name)
+    except ModuleNotFoundError:
+        print(f"No day '{opts.day}' found!", file=sys.stderr)
+        return 1
+    except AttributeError:
+        print(
+            f"Day '{opts.day}' does not define '{solver_name}'", file=sys.stderr
+        )
+        return 1
+
+    input_ = opts.input or (
+        Path(module.__file__).parent
+        / ("test.txt" if opts.test else "input.txt")
+    )
+    print(solver()(input_))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ aoc2023/utils/solver.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any, Optional
+
+
+class Solver:
+    _STRIP: bool = True
+
+    def __init__(self) -> None:
+        self._accumulator = 0
+
+    def __init_subclass__(cls, strip: bool = True) -> None:
+        cls._STRIP = strip
+
+    @abstractmethod
+    def _process_line(self, line: str) -> Optional[int]:
+        pass
+
+    def postprocess(self) -> None:
+        pass
+
+    def __call__(self, path: Path) -> int:
+        with path.open() as f:
+            for line in f.readlines():
+                value = self._process_line(
+                    line.strip() if self._STRIP else line
+                )
+                if value is not None:
+                    self._accumulator += value
+
+        self.postprocess()
+        return self._accumulator
